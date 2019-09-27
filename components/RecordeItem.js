09b31bbd3193e5da8a109337819d52f2475c91ac
Altin_Vitija <altin.vitija@riinvest.net>
2019-09-27 17:11:41 +0200

    Record

---
+++ components/RecordeItem.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+export default class RecordeItem extends Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+
+                <View style={{ height: 'auto', paddingRight: 330 }}>
+                    <Text style={{ fontSize: 17, marginTop: -35 }}   > {this.props.rank} </Text>
+                </View>
+                <View style={{ paddingLeft: 40, paddingRight: 40 }}>
+                    <Text style={{ fontSize: 17, marginTop: -35 }}> {this.props.name} </Text>
+                </View>
+                <View style={{ paddingLeft: 330 }}>
+                    <Text style={{ fontSize: 17, marginTop: -35 }}> {this.props.record} </Text>
+                </View>
+            </View >
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+        marginTop: 50,
+        width: -1,
+        backgroundColor: '#2EA0D1'
+
+    },
+})
--- screens/RecordScreen.js
+++ screens/RecordScreen.js
@@ -1,54 +1,65 @@
 import React from 'react';
-import { Text, View ,StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
 import axios from 'axios';
+import RecordeItem from '../components/RecordeItem';
 
 export default class RecordScreen extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          record:[],
-          users:[],
-        }
-      }
-   
-      
-      componentDidMount() {
+  static navigationOptions = {
 
-        axios.get(`/users/record/id`)
-        .then(response=>{
-          this.setState({record:response.users.response});
-            console.log(response)
-        })
-        .catch(error => {
-          console.log(error);
-        });
-console.log(this.state.record.users)
+    title: 'Top Record'
+
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      records: [],
+      users: ""
     }
+  }
+
+  componentDidMount() {
+
+    axios.get(`/records/`)
+      .then(response => {
+        const user = response.data;
+        alert(JSON.stringify(user))
+
+        this.setState({ records: user })
 
 
+      })
+      .catch(error =>
+        console.log(error)
+      );
 
+  }
 
-  render(){
+  render() {
 
-    return(
-      <View style={styles.container}>
-        <Text style={{fontSize:25}}>
-            {this.props.record}
-            {this.props.users}
-          </Text>       
-      </View>
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          {
+            this.state.records.length !== 0 ?
+              this.state.records.map((record, index) => (
+                <RecordeItem key={index} rank={record.rank} name={record.name} record={record.record} > </RecordeItem>
+              )
+              ) : null
+          }
+
+        </View>
+      </ScrollView>
     );
   }
 }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      justifyContent:'center',
-      alignItems:'center',
-  
-    },
-    
-  });
-  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+
+});
