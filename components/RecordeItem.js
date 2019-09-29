1f3ccd94c6424ccc8e5ba4b21f980e070e7aba45
Altin_Vitija <altin.vitija@riinvest.net>
2019-09-29 17:58:59 +0200

    ttt

--- components/RecordeItem.js
+++ components/RecordeItem.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
-
+import { Ionicons } from '@expo/vector-icons';
 export default class RecordeItem extends Component {
 
     render() {
         return (
+
             <View style={styles.container}>
 
-                <View style={{ height: 'auto', paddingRight: 330, backgroundColor: '#2EA0D1', marginLeft: 10, marginTop: 30 }}>
-                    <Text style={{ fontSize: 17, marginTop: -35 }}   > {this.props.rank} </Text>
-                </View>
-                <View style={{ paddingLeft: 40, paddingRight: 40 }}>
-                    <Text style={{ fontSize: 17, marginTop: -35 }}> {this.props.name} </Text>
-                </View>
-                <View style={{ paddingLeft: 330 }}>
-                    <Text style={{ fontSize: 17, marginTop: -35 }}> {this.props.record} </Text>
+
+                <View style={styles.blloki}>
+
+                    <View style={{
+                        marginRight: 250, marginLeft: 10, borderRadius: 20, height: 'auto', marginTop: 5
+
+                    }}>
+
+
+                        <Ionicons name="md-trophy" size={32} color="gold" />
+
+                    </View>
+
+                    <View style={{ paddingLeft: 40, paddingRight: 40, height: 'auto' }}>
+                        <Text style={{
+                            fontSize: 17, marginTop: -25, justifyContent: 'center', alignItems: 'center', color: 'white'
+                        }}> {this.props.name} </Text>
+                    </View>
+
+                    <View style={{ marginLeft: 250, height: 'auto' }}>
+                        <Text style={{
+                            fontSize: 17, marginTop: -25, color: 'white'
+                        }}> {this.props.record} </Text>
+                    </View>
+
+                    <View style={{ marginLeft: 40, height: 'auto' }}>
+                        <Text style={{
+                            fontSize: 50, marginTop: -1735, marginLeft: 70, color: 'white'
+                        }}> {this.props.myRank} </Text>
+                    </View>
+
                 </View>
             </View >
+
         )
     }
 }
@@ -26,9 +51,24 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         flex: 1,
-        marginTop: 50,
-        width: -1,
-        backgroundColor: '#2EA0D1'
+
 
     },
+    blloki: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+        marginTop: 50,
+        height: 50,
+        backgroundColor: '#2EA0D1',
+        borderRadius: 20,
+        width: 300,
+
+
+
+    },
+
+
+
+
 })
--- screens/RecordScreen.js
+++ screens/RecordScreen.js
@@ -3,6 +3,7 @@ import { Text, View, StyleSheet, ScrollView } from 'react-native';
 import axios from 'axios';
 import RecordeItem from '../components/RecordeItem';
 
+
 export default class RecordScreen extends React.Component {
   static navigationOptions = {
 
@@ -38,14 +39,22 @@ export default class RecordScreen extends React.Component {
 
     return (
       <ScrollView>
+        <View style={styles.rankuu}>
+          <Text style={{ marginLeft: 50, marginTop: 70, fontSize: 30, color: 'white' }}>My Rank is :</Text>
+
+        </View>
+
         <View style={styles.container}>
           {
             this.state.records.length !== 0 ?
               this.state.records.map((record, index) => (
-                <RecordeItem key={index} rank={record.rank} name={record.name} record={record.record} > </RecordeItem>
+
+                <RecordeItem key={index} rank={record.rank} name={record.name} record={record.record} myRank={record.myRank}
+                >  </RecordeItem>
               )
               ) : null
           }
+
 
         </View>
       </ScrollView>
@@ -59,7 +68,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'white',
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+
 
   },
+  rankuu: {
+    width: 360,
+    height: 200,
+    backgroundColor: '#2EA0D1',
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    borderTopRightRadius: 0,
+    borderTopLeftRadius: 0,
+
+
+
+
+
+  },
+
 
 });
